f6bb7573f7fe9a93f9bba429bb5f992a3f0ea3db
FaraKhalili <FaraKhalili@github.com>
2020-11-13 12:03:38 -0600

    Merge two list with unique names

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Merge_two_list_with_unique_names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string[] x = { "ali", "zafar", "goli", "amin", "nikita" };
+            string[] y = { "ali", "hassan", "amin" };
+            List<string> xl = new List<string>(x);
+            List<string> yl = new List<string>(y);
+
+
+            foreach (string itemx in xl)
+            {
+                Console.WriteLine(itemx);
+            }
+            foreach (string item in yl)
+            {
+                if (!xl.Contains(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+        }
+    }
+}
